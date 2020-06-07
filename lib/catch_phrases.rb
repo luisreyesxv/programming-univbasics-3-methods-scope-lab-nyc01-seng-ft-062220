94ccacdb97eab51d316cbc3a512d85251e242352
Luis Reyes <luisreyesxv@yahoo.com>
2020-06-07 01:27:10 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,9 @@
-def mario
+def mario(phrase)
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
 
-def toadstool
+def toadstool(phrase)
+  mario(phrase)
   puts status
 end
